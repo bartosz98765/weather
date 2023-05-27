94da954f5e9d187aa411ae4dd7857c6aa0f176b4
bartek <bartek@grupazpr.pl>
2023-05-27 17:21:27 +0200

    added correct current and forecast data from api

--- backend/test_data/api_data.py
+++ backend/test_data/api_data.py
@@ -1,111 +1,143 @@
-FORECAST_FROM_API = {
-    "location": {
-        "name": "Bialystok",
-        "region": "",
-        "country": "Poland",
-        "lat": 53.13,
-        "lon": 23.08,
-        "tz_id": "Europe/Warsaw",
-        "localtime_epoch": 1685198904,
-        "localtime": "2023-05-27 16:48",
+CURRENT_AND_FORECAST_FROM_API_2_DAYS = {
+  "location": {
+    "name": "Bialystok",
+    "region": "",
+    "country": "Poland",
+    "lat": 53.13,
+    "lon": 23.08,
+    "tz_id": "Europe/Warsaw",
+    "localtime_epoch": 1685200637,
+    "localtime": "2023-05-27 17:17"
+  },
+  "current": {
+    "last_updated_epoch": 1685200500,
+    "last_updated": "2023-05-27 17:15",
+    "temp_c": 14.9,
+    "temp_f": 58.8,
+    "is_day": 1,
+    "condition": {
+      "text": "Sunny",
+      "icon": "//cdn.weatherapi.com/weather/64x64/day/113.png",
+      "code": 1000
     },
-    "current": {
-        "last_updated_epoch": 1685198700,
-        "last_updated": "2023-05-27 16:45",
-        "temp_c": 16.8,
-        "temp_f": 62.2,
-        "is_day": 1,
-        "condition": {
+    "wind_mph": 10.5,
+    "wind_kph": 16.9,
+    "wind_degree": 22,
+    "wind_dir": "NNE",
+    "pressure_mb": 1026,
+    "pressure_in": 30.28,
+    "precip_mm": 0,
+    "precip_in": 0,
+    "humidity": 49,
+    "cloud": 9,
+    "feelslike_c": 13.8,
+    "feelslike_f": 56.8,
+    "vis_km": 10,
+    "vis_miles": 6,
+    "uv": 4,
+    "gust_mph": 13.9,
+    "gust_kph": 22.3
+  },
+  "forecast": {
+    "forecastday": [
+      {
+        "date": "2023-05-27",
+        "date_epoch": 1685145600,
+        "day": {
+          "maxtemp_c": 17.3,
+          "maxtemp_f": 63.1,
+          "mintemp_c": 4.9,
+          "mintemp_f": 40.8,
+          "avgtemp_c": 11.3,
+          "avgtemp_f": 52.3,
+          "maxwind_mph": 13.9,
+          "maxwind_kph": 22.3,
+          "totalprecip_mm": 0,
+          "totalprecip_in": 0,
+          "totalsnow_cm": 0,
+          "avgvis_km": 10,
+          "avgvis_miles": 6,
+          "avghumidity": 59,
+          "daily_will_it_rain": 0,
+          "daily_chance_of_rain": 0,
+          "daily_will_it_snow": 0,
+          "daily_chance_of_snow": 0,
+          "condition": {
             "text": "Sunny",
             "icon": "//cdn.weatherapi.com/weather/64x64/day/113.png",
-            "code": 1000,
+            "code": 1000
+          },
+          "uv": 4
         },
-        "wind_mph": 11.6,
-        "wind_kph": 18.7,
-        "wind_degree": 21,
-        "wind_dir": "NNE",
-        "pressure_mb": 1025.0,
-        "pressure_in": 30.28,
-        "precip_mm": 0.0,
-        "precip_in": 0.0,
-        "humidity": 45,
-        "cloud": 16,
-        "feelslike_c": 16.8,
-        "feelslike_f": 62.2,
-        "vis_km": 10.0,
-        "vis_miles": 6.0,
-        "uv": 5.0,
-        "gust_mph": 13.4,
-        "gust_kph": 21.6,
-    },
-    "forecast": {
-        "forecastday": [
-            {
-                "date": "2023-05-27",
-                "date_epoch": 1685145600,
-                "day": {
-                    "maxtemp_c": 17.3,
-                    "maxtemp_f": 63.1,
-                    "mintemp_c": 4.9,
-                    "mintemp_f": 40.8,
-                    "avgtemp_c": 11.3,
-                    "avgtemp_f": 52.3,
-                    "maxwind_mph": 13.9,
-                    "maxwind_kph": 22.3,
-                    "totalprecip_mm": 0.0,
-                    "totalprecip_in": 0.0,
-                    "totalsnow_cm": 0.0,
-                    "avgvis_km": 10.0,
-                    "avgvis_miles": 6.0,
-                    "avghumidity": 59.0,
-                    "daily_will_it_rain": 0,
-                    "daily_chance_of_rain": 0,
-                    "daily_will_it_snow": 0,
-                    "daily_chance_of_snow": 0,
-                    "condition": {
-                        "text": "Sunny",
-                        "icon": "//cdn.weatherapi.com/weather/64x64/day/113.png",
-                        "code": 1000,
-                    },
-                    "uv": 4.0,
-                },
-                "astro": {},
-                "hour": [],
-            },
-            {
-                "date": "2023-05-28",
-                "date_epoch": 1685232000,
-                "day": {
-                    "maxtemp_c": 21.9,
-                    "maxtemp_f": 71.4,
-                    "mintemp_c": 5.5,
-                    "mintemp_f": 41.9,
-                    "avgtemp_c": 13.8,
-                    "avgtemp_f": 56.9,
-                    "maxwind_mph": 5.6,
-                    "maxwind_kph": 9.0,
-                    "totalprecip_mm": 0.0,
-                    "totalprecip_in": 0.0,
-                    "totalsnow_cm": 0.0,
-                    "avgvis_km": 10.0,
-                    "avgvis_miles": 6.0,
-                    "avghumidity": 56.0,
-                    "daily_will_it_rain": 0,
-                    "daily_chance_of_rain": 0,
-                    "daily_will_it_snow": 0,
-                    "daily_chance_of_snow": 0,
-                    "condition": {
-                        "text": "Sunny",
-                        "icon": "//cdn.weatherapi.com/weather/64x64/day/113.png",
-                        "code": 1000,
-                    },
-                    "uv": 4.0,
-                },
-                "astro": {},
-                "hour": [],
-            },
-        ]
-    },
+        "astro": {},
+        "hour": []
+      },
+      {
+        "date": "2023-05-28",
+        "date_epoch": 1685232000,
+        "day": {
+          "maxtemp_c": 21.9,
+          "maxtemp_f": 71.4,
+          "mintemp_c": 5.5,
+          "mintemp_f": 41.9,
+          "avgtemp_c": 13.8,
+          "avgtemp_f": 56.9,
+          "maxwind_mph": 5.6,
+          "maxwind_kph": 9,
+          "totalprecip_mm": 0,
+          "totalprecip_in": 0,
+          "totalsnow_cm": 0,
+          "avgvis_km": 10,
+          "avgvis_miles": 6,
+          "avghumidity": 56,
+          "daily_will_it_rain": 0,
+          "daily_chance_of_rain": 0,
+          "daily_will_it_snow": 0,
+          "daily_chance_of_snow": 0,
+          "condition": {
+            "text": "Sunny",
+            "icon": "//cdn.weatherapi.com/weather/64x64/day/113.png",
+            "code": 1000
+          },
+          "uv": 4
+        },
+        "astro": {},
+        "hour": []
+      },
+      {
+        "date": "2023-05-29",
+        "date_epoch": 1685318400,
+        "day": {
+          "maxtemp_c": 21.2,
+          "maxtemp_f": 70.2,
+          "mintemp_c": 8.1,
+          "mintemp_f": 46.6,
+          "avgtemp_c": 15.5,
+          "avgtemp_f": 59.9,
+          "maxwind_mph": 5.8,
+          "maxwind_kph": 9.4,
+          "totalprecip_mm": 0,
+          "totalprecip_in": 0,
+          "totalsnow_cm": 0,
+          "avgvis_km": 10,
+          "avgvis_miles": 6,
+          "avghumidity": 62,
+          "daily_will_it_rain": 0,
+          "daily_chance_of_rain": 0,
+          "daily_will_it_snow": 0,
+          "daily_chance_of_snow": 0,
+          "condition": {
+            "text": "Partly cloudy",
+            "icon": "//cdn.weatherapi.com/weather/64x64/day/116.png",
+            "code": 1003
+          },
+          "uv": 5
+        },
+        "astro": {},
+        "hour": []
+      }
+    ]
+  }
 }
 
 HISTORY_FROM_API_DAY_5 = {
@@ -327,3 +359,4 @@ HISTORY_FROM_API_DAY_1 = {
     ]
   }
 }
+
