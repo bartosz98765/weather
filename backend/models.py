09f6444087c1ee87a8c15b2a72fda9ecb03c1b09
bartek <bartek@grupazpr.pl>
2023-05-28 12:33:28 +0200

    added models

--- backend/models.py
+++ backend/models.py
@@ -1,3 +1,34 @@
 from django.db import models
 
-# Create your models here.
+
+class Location(models.Model):
+    name = models.CharField(max_length=64)
+    country = models.CharField(max_length=64)
+    latitude = models.FloatField()
+    longitude = models.FloatField()
+    timezone = models.CharField(max_length=64)
+
+    def __str__(self):
+        return f"{self.name} / {self.country}"
+
+
+class CurrentWeather(models.Model):
+    time_epoch = models.IntegerField()
+    temp_c = models.FloatField()
+    wind_kph = models.FloatField()
+    wind_dir = models.CharField(max_length=3)
+    pressure_mb = models.IntegerField()
+    precip_mm = models.IntegerField()
+    humidity = models.IntegerField()
+    condition = models.CharField(max_length=256)
+
+
+class DailyWeather(models.Model):
+    time_epoch = models.IntegerField()
+    maxtemp_c = models.FloatField()
+    mintemp_c = models.FloatField()
+    avgtemp_c = models.FloatField()
+    maxwind_kph = models.FloatField()
+    totalprecip_mm = models.IntegerField()
+    avghumidity = models.IntegerField()
+    condition = models.CharField(max_length=256)
