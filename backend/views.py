573dc3db55d868443dd043d1d6cffd8ba2e2f9c6
bartek <bartek@grupazpr.pl>
2023-05-28 09:27:37 +0200

    test: assert with response json - ok: get_context_data mocked with response json

--- backend/views.py
+++ backend/views.py
@@ -1,5 +1,3 @@
-import json
-
 from django.http import JsonResponse
 from django.views.generic import ListView
 
@@ -10,6 +8,15 @@ class MainView(ListView):
     def get_queryset(self):
         return []
 
+    def get_context_data(self, **kwargs):
+        context = super().get_context_data(**kwargs)
+        context["location"] = EXPECTED_WEATHER_DATA["location"]
+        context["current"] = EXPECTED_WEATHER_DATA["current"]
+        context["daily"] = EXPECTED_WEATHER_DATA["daily"]
+        return context
+
     def render_to_response(self, context, **response_kwargs):
-        data = EXPECTED_WEATHER_DATA
-        return JsonResponse(data, safe=False)
+        weather_data = {"location": context["location"]}
+        weather_data.update({"current": context["current"]})
+        weather_data.update({"daily": context["daily"]})
+        return JsonResponse(weather_data, safe=False)
