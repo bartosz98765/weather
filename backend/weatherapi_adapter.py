6ac7454e1d889f5361868bbf0ba2f3feef4c0cb9
bartek <bartek@grupazpr.pl>
2023-05-29 19:18:12 +0200

    weatherapi adapter schema

---
+++ backend/weatherapi_adapter.py
@@ -0,0 +1,41 @@
+from marshmallow import Schema, fields, pre_load
+
+
+class LocationSchema(Schema):
+    name = fields.String(required=True)
+    country = fields.String(required=False)
+    latitude = fields.Float(required=True, load_from='lat')
+    longitude = fields.Float(required=True, load_from='lon')
+    timezone = fields.String(required=True, load_from='tz_id')
+
+
+class CurrentWeather(Schema):
+    time_epoch = fields.Integer(required=True, load_from='last_updated_epoch')
+    temp_c = fields.Float(required=True)
+    wind_kph = fields.Float(required=False)
+    wind_dir = fields.String(required=False)
+    pressure_mb = fields.Integer(required=False)
+    precip_mm = fields.Integer(required=True)
+    humidity = fields.Integer(required=False)
+    condition = fields.String(required=True)
+
+    @pre_load
+    def get_condition(self, data):
+        data["condition"] = data["condition"]["text"]
+        return data
+
+
+class DailyWeather(Schema):
+    time_epoch = fields.Integer(required=True)
+    maxtemp_c = fields.Float(required=True)
+    mintemp_c = fields.Float(required=True)
+    avgtemp_c = fields.Float(required=False)
+    maxwind_kph = fields.Float(required=False)
+    totalprecip_mm = fields.Integer(required=True)
+    avghumidity = fields.Integer(required=False)
+    condition = fields.String(required=True)
+
+    @pre_load
+    def get_condition(self, data):
+        data["condition"] = data["condition"]["text"]
+        return data
